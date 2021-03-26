c1af46c234265f4caba7e63af0dbac8aafa833b6
ItzelDaniela <81449612+ItzelDaniela@users.noreply.github.com>
2021-03-26 17:56:54 -0600

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,10 @@
+const imagenes = document.querySelectorAll('.img-Especies')
+const imagenesLightx = document.querySelector('.agregar-imagen')
+const ContenedorLight = document.querySelector('.imagen-light')
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        alert("auch, me doli&acuteo")
+    })
+})
